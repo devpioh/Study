cdb16109639b6763d3c8d33fc75b0a7b04c2f45b
pioh <devpioh@gmail.com>
2019-01-31 22:51:04 +0900

    coroutine test

---
+++ Python/FileSorter/TestCoroutine.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import time
+import asyncio
+import random
+from tqdm import tqdm
+
+
+class job:
+    def __init__(self, value):
+        self.value = value
+
+    async def work(self):
+        print("start : %d" % self.value)
+        await asyncio.sleep(self.value)
+        print("end : %d" % self.value)
+        return self.value
+
+async def jobLoop(works):
+    for w in asyncio.as_completed(works):
+        c = await w
+    
+    print("finished jobs")
+
+async def progressBar(works):
+    # for w in tqdm( asyncio.as_completed(works), total=len(works) ):
+    for w in tqdm( works, total=len(works) ):
+        await w
+
+works = list()
+for i in range( 1, 6 ):
+    j = job( random.randint(i, 6) )
+    works.append( asyncio.ensure_future(j.work()) )
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete( asyncio.gather( jobLoop(works), progressBar(works) ) )
+loop.close()
+
+
